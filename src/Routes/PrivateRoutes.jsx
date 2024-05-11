62a9bd23d40676d5592941c4f7512d9dd86a3c32
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2024-05-12 02:15:49 +0600

    Add loading spinners in Private Spinners from React Js Spinner

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,9 +1,26 @@
-const PrivateRoutes = () => {
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { PacmanLoader } from "react-spinners";
+
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+    if (loading) {
+        return <div className="flex justify-center items-center text-[#ea9b25] min-h-screen"><PacmanLoader color="#36d7b7" /></div>;
+    }
+    if (user) {
+        return children;
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+            <Navigate state={location.pathname} to="/login"></Navigate>
+        </>
     );
 };
 
+PrivateRoutes.propTypes = {
+    children: PropTypes.node,
+};
 export default PrivateRoutes;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import { PacmanLoader } from 'react-spinners';
 const Home = () => {
     return (
         <div>
             <h1>Hi from Home page</h1>
+            <PacmanLoader color="#ea9b25" />
         </div>
     );
 };
